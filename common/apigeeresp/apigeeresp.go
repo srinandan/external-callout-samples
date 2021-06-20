8fc5b335224b9032d905aad7786fc28c89098a5c
srinandan <nandanks@gmail.com>
2021-06-19 17:48:56 -0700

    common funcs

---
+++ common/apigeeresp/apigeeresp.go
@@ -0,0 +1,34 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package response
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/srinandan/external-callout-samples/common/pkg/apigee"
+	"github.com/srinandan/sample-apps/common"
+)
+
+func GetErrorMessage(errMessage string) string {
+	msg, _ := json.Marshal(common.ErrorMessage{StatusCode: http.StatusInternalServerError, Message: errMessage})
+	return string(msg)
+}
+
+func SetBoolFlowVariable(result bool) *apigee.FlowVariable {
+	return &apigee.FlowVariable{
+		Value: &apigee.FlowVariable_Bool{result},
+	}
+}
